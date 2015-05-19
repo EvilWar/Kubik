e14e2638fac9625adae55a224f61bcab7fd81b8a
Denis <den00789@gmail.com>
2015-05-19 22:48:56 +0500

    no message

---
+++ New Unity Project 8/Assets/Release/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public int score;
+	public int magnets;
+	public int bombs;
+	public int slows;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
---
+++ New Unity Project 8/Assets/Release/Scripts/PlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+	public float speed;
+	public float up;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		Vector2 mpos;
+
+		mpos = Vector2.zero;
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+			mpos = Camera.main.ScreenToWorldPoint(touch.position);
+
+			mpos.y += up;
+			float step = speed * Time.fixedDeltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, mpos, step);
+		}
+		else
+		{
+			if (Input.GetMouseButton(0)) 
+			{
+				mpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			
+				mpos.y += up;
+				float step = speed * Time.fixedDeltaTime;
+				transform.position = Vector3.MoveTowards(transform.position, mpos, step);
+
+			}
+
+		}
+	}
+}
